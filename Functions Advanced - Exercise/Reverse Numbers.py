637ff3ff5de460349c1373145bd2d12af8828f8e
Lev <106928252+Quanitte@users.noreply.github.com>
2023-10-02 22:40:57 +0300

    Delete Functions Advanced - Exercise directory

--- Functions Advanced - Exercise/Reverse Numbers.py
+++
@@ -1,9 +0,0 @@
-input_string = input()
-integers = list(map(int, input_string.split()))
-stack = []
-for num in integers:
-    stack.append(num)
-reversed_integers = []
-while stack:
-    reversed_integers.append(stack.pop())
-print(" ".join(map(str, reversed_integers)))
