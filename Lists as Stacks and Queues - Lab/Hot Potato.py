16134dc2a5a90ecd8ea2c36129466dbf9488d194
Lev <106928252+Quanitte@users.noreply.github.com>
2023-10-02 22:41:03 +0300

    Delete Lists as Stacks and Queues - Lab directory

--- Lists as Stacks and Queues - Lab/Hot Potato.py
+++
@@ -1,9 +0,0 @@
-kids = input().split()
-n = int(input())
-circle = kids
-while len(circle) > 1:
-    index_to_remove = (n - 1) % len(circle)
-    removed_kid = circle.pop(index_to_remove)
-    print(f"Removed {removed_kid}")
-    circle = circle[index_to_remove:] + circle[:index_to_remove]
-print(f"Last is {circle[0]}")
